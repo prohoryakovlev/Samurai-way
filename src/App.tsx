a6cb78d9659f196f802e49452364eecc7060620d
Yakovlev Prohor <prohor-yakovlev@mail.ru>
2023-12-10 21:07:14 +0300

    Change Components.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Tehnologies";
+import Header from "./Headers";
+
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+        <Header/>
+        <Technologies/>
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/Tehnologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tehnologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+export default Tehnologies
